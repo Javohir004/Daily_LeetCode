88c0bab00afd1f23a11b5d6423ee92e7fa18f02a
Javohir004 <javohirsharifjonov795@gmail.com>
2025-08-27 16:26:21 +0500

    Dupclicte emaillarni olib tashlash

---
+++ src/main/java/uz/jvh/daily_leetcode/Day_3.java
@@ -0,0 +1,11 @@
+package uz.jvh.daily_leetcode;
+
+public class Day_3 {
+    // DataBase
+    //   type = easy
+    //   num: 182. Duplicate Emails
+
+    // SELECT person from Email GROUP BY email HAVING COUNT(email) > 1
+
+
+}
