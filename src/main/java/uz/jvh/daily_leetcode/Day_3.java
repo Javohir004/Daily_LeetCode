096f582a02741d3bc8b74f555a11af832edae885
Javohir004 <javohirsharifjonov795@gmail.com>
2025-08-27 16:41:39 +0500

    Dupclicte emaillarni olib tashlash keyingisi cutmoer va order jadvalidan foydalain hech qachon order bermagan userlarni topish

--- src/main/java/uz/jvh/daily_leetcode/Day_3.java
+++ src/main/java/uz/jvh/daily_leetcode/Day_3.java
@@ -4,8 +4,12 @@ public class Day_3 {
     // DataBase
     //   type = easy
     //   num: 182. Duplicate Emails
+    //   num : 183.Customers Who Never Order
 
     // SELECT person from Email GROUP BY email HAVING COUNT(email) > 1
 
+    // SELECT c.name as Customer from Customers c
+    // LEFT JOIN Orders o On o.CustomerId = c.Id
+    // where o.customerId is Null
 
 }
