0666538d07dcf44c27ba233a7822b632ff10fad9
Javohir004 <javohirsharifjonov795@gmail.com>
2025-08-29 09:11:28 +0500

    string da berilgan raqamlarni qo'shish kerak edi

--- src/main/java/uz/jvh/daily_leetcode/DailyLeetCodeApplication.java
+++ src/main/java/uz/jvh/daily_leetcode/DailyLeetCodeApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import static uz.jvh.daily_leetcode.Day_1.day1;
 import static uz.jvh.daily_leetcode.Day_2.day_2;
+import static uz.jvh.daily_leetcode.Day_4.day_4;
 
 @SpringBootApplication
 public class DailyLeetCodeApplication {
@@ -12,8 +13,8 @@ public class DailyLeetCodeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DailyLeetCodeApplication.class, args);
 		// day1();
-
-       day_2();
+		// day_2();
+		// day_4();
 	}
 
 }
