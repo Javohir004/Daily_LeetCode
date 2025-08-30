6f8c56623109882074710f783406020128959d0b
Javohir004 <javohirsharifjonov795@gmail.com>
2025-08-30 23:17:17 +0500

    2 chi eng katta oylik ni topish

---
+++ src/main/java/uz/jvh/daily_leetcode/Day_5.java
@@ -0,0 +1,12 @@
+package uz.jvh.daily_leetcode;
+
+public class Day_5 {
+
+    // Name: DataBase
+    // type: medium
+    // num: 176. Second Highest Salary
+
+
+    //SELECT (SELECT DISTINCT salary FROM Employee
+    //        ORDER BY salary DESC LIMIT 1 OFFSET 1) AS SecondHighestSalary;
+}
