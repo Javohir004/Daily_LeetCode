3d8639021474aed68ca80af35ed193cd801906e7
Javohir004 <javohirsharifjonov795@gmail.com>
2025-08-31 20:17:54 +0500

    chunish juda qiyin bo'lyapti buni

--- src/main/java/uz/jvh/daily_leetcode/Day_6.java
+++ src/main/java/uz/jvh/daily_leetcode/Day_6.java
@@ -8,25 +8,48 @@ public class Day_6 {
 
 
     public static void day_6(){
-
         String s = "babad";
-
         String longest = "";
+
         for (int i = 0; i < s.length(); i++) {
-
-            for (int j = i + 1; j <= s.length(); j++) {
-                String sub = s.substring(i, j);
-                String reversed = new StringBuilder(sub).reverse().toString();
-
-                /// bu yerdan reverse ga teng va oldinggisan uzun bo'lsin sharti bor
-                if (sub.equals(reversed) && sub.length() > longest.length()) {
-                    longest = sub;
+            for (int j = i; j < s.length(); j++) {
+                boolean isPal = true;
+                for (int l = i, r = j; l < r; l++, r--) {
+                    if (s.charAt(l) != s.charAt(r)) {
+                        isPal = false;
+                        break;
+                    }
                 }
-
+                if (isPal && (j - i + 1) > longest.length()) {
+                    longest = s.substring(i, j + 1);
+                }
             }
         }
+
         System.out.println(longest);
     }
+
+//    public static void day_6(){
+//
+//        String s = "babad";
+//
+//        String longest = "";
+//        for (int i = 0; i < s.length(); i++) {
+//
+//            for (int j = i + 1; j <= s.length(); j++) {
+//                String sub = s.substring(i, j);
+//                String reversed = new StringBuilder(sub).reverse().toString();
+//
+//                /// bu yerdan reverse ga teng va oldinggisan uzun bo'lsin sharti bor
+//                if (sub.equals(reversed) && sub.length() > longest.length()) {
+//                    longest = sub;
+//                }
+//
+//            }
+//        }
+//        System.out.println(longest);
+//    }
+
 
 
 }
