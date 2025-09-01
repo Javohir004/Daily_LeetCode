d1b2ab28ef93534e66b60558688b8d20b1bb88e2
Javohir004 <javohirsharifjonov795@gmail.com>
2025-09-01 21:20:30 +0500

    duplicate emaillarni o'chirish

--- src/main/java/uz/jvh/daily_leetcode/Day_7.java
+++ src/main/java/uz/jvh/daily_leetcode/Day_7.java
@@ -2,4 +2,9 @@ package uz.jvh.daily_leetcode;
 
 public class Day_7 {
 
+    /// Name: DataBase
+    /// Type: Easy
+    /// Num: 196. Delete Duplicate Emails
+
+    /// Delete p1 from Person p1 Join Person p2 on p1.email = p2.email and p1.id > p2.id;
 }
