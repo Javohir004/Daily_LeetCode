3231f19f477d6589032551de21255ffe80477a16
Javohir004 <javohirsharifjonov795@gmail.com>
2025-09-23 18:40:26 +0500

    .

--- src/main/java/uz/jvh/daily_leetcode/DailyLeetCodeApplication.java
+++ src/main/java/uz/jvh/daily_leetcode/DailyLeetCodeApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import static uz.jvh.daily_leetcode.Day_1.day1;
 import static uz.jvh.daily_leetcode.Day_2.day_2;
+import static uz.jvh.daily_leetcode.Day_29.day_29;
 import static uz.jvh.daily_leetcode.Day_4.day_4;
 import static uz.jvh.daily_leetcode.Day_6.day_6;
 import static uz.jvh.daily_leetcode.Day_8.day_8;
@@ -19,7 +20,9 @@ public class DailyLeetCodeApplication {
 		// day_4();
 		// day_6();
 		// day_8();
-		day_9();
+		// day_9();
+
+		day_29();
 	}
 
 }
