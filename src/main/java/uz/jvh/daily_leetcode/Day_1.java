4ced5c72a22e05eb6c0ffe22af45d8f3d771a0c1
Sharifjonov Javohir <javohir_sharifjonov@epam.com>
2026-08-27 11:22:17 +0500

    twu sum

--- src/main/java/uz/jvh/daily_leetcode/Day_1.java
+++ src/main/java/uz/jvh/daily_leetcode/Day_1.java
@@ -1,31 +1,24 @@
 package uz.jvh.daily_leetcode;
 
+
 import java.util.Arrays;
 
 public class Day_1 {
 
-    /** 8-AVGUST 2025-YIL.
-     * Type=Hard ,
-     Array Nomer 4 ;
-       Given two sorted arrays nums1 and nums2 of size m and n respectively,
-     return the median of the two sorted arrays.
-     The overall run time complexity should be O(log (m+n)). **/
+    /** 27-AVGUST 2025-YIL.
+     * Array , 1.Two Sum **/
     public static void day1(){
-        int[] nums1 = {1, 3, 5};
-        int[] nums2 = {2, 4, 6, 8};
+        int[] nums = {2,7,11,15};
+        int target = 9;
 
-        int[] array = new int[nums1.length + nums2.length];
-
-        System.arraycopy(nums1,0,array, 0 ,nums1.length);
-        System.arraycopy(nums2,0,array, nums1.length ,nums2.length);
-        Arrays.sort(array);
-
-        int n = array.length;
-
-        if(n % 2 == 1){
-            System.out.println(array[n/2]);
-        }else{
-            System.out.println((array[n/2] + array[n/2-1])/2);
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j = i+1; j < nums.length-1 ; j++) {
+             if (nums[i] + nums[j] == target) {
+                 System.out.println(Arrays.toString(new int[] {i, j}));
+             return;
+             }
+            }
         }
+        System.out.println(Arrays.toString(new int[] {}));
     }
 }
