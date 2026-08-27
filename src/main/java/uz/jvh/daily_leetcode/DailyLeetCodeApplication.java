362501a5a748472b82692068265156eb0b15c1b9
Sharifjonov Javohir <javohir_sharifjonov@epam.com>
2026-08-27 10:53:47 +0500

    here we go again

--- src/main/java/uz/jvh/daily_leetcode/DailyLeetCodeApplication.java
+++ src/main/java/uz/jvh/daily_leetcode/DailyLeetCodeApplication.java
@@ -2,26 +2,12 @@ package uz.jvh.daily_leetcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import static uz.jvh.daily_leetcode.Day_1.day1;
-import static uz.jvh.daily_leetcode.Day_2.day_2;
-import static uz.jvh.daily_leetcode.Day_29.day_29;
-import static uz.jvh.daily_leetcode.Day_4.day_4;
-import static uz.jvh.daily_leetcode.Day_6.day_6;
-import static uz.jvh.daily_leetcode.Day_8.day_8;
-import static uz.jvh.daily_leetcode.Day_9.day_9;
 
 @SpringBootApplication
 public class DailyLeetCodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DailyLeetCodeApplication.class, args);
-		// day1();
-		// day_2();
-		// day_4();
-		// day_6();
-		// day_8();
-		// day_9();
-		   day_29();
 	}
 
 }
